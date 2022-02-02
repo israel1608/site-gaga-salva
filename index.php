eb666362664d0fc34196af0f41840cbe5bb0ff9c
israel costa <israel_dopara@hotmail.com>
2022-02-02 12:21:46 -0300

    responsividade fotos pronto

--- index.php
+++ index.php
@@ -26,151 +26,136 @@ include("data.php");
     -->
 </head>
 <body>
-     <div class="fixed">
+    <div class="fixed">
         <div class="header">
-        
             <span>SÓ O GAGÁ SALVA</span>
-        <!--  <div class="login">
-                <a href="#">Log in</a>
-            </div>-->
         </div>
-            <div class="menu">
-                <div class="mini-menu">
-                    <a class="foto" onclick="fecharLogin()" href="#foto">
-                        <img src="icones/logo.png"alt="#"width=61 height=40>
-                    </a>
-                    <a onclick="fecharLogin()" class="ativo" id="link" href="#inicio">início</a>
-                    <a class="ativo" onclick="fecharLogin()" href="#participantes">participantes</a>
-                    <a class="ativo" onclick="fecharLogin()" href="#gametag">gametags</a>
-                    <a class="ativo" onclick="fecharLogin()" href="#arquivos">arquivos</a>
-                    <!--<a class="ativo" href="#podcast">podcast</a>-->
-                    <!-- <a class="ativo" onclick="fecharLogin()" href="#fotos">fotos</a>-->
-                    <!--<a class="ativo" href="#">videos</a>--> 
-                    <a class="login" onclick="abrirLogin()" href="#login">Login</a>
+        <div class="menu">
+            <div class="mini-menu">
+                <a class="foto" onclick="fecharLogin()" href="#foto">
+                    <img src="icones/logo.png"alt="#"width=61 height=40>
+                </a>
+                <a onclick="fecharLogin()" class="ativo" id="link" href="#inicio">início</a>
+                <a class="ativo" onclick="fecharLogin()" href="#participantes">participantes</a>
+                <a class="ativo" onclick="fecharLogin()" href="#gametag">gametags</a>
+                <a class="ativo" onclick="fecharLogin()" href="#arquivos">arquivos</a>
+            
+                <a class="login" onclick="abrirLogin()" href="#login">Login</a>
+            </div>
+        </div>
+    </div>
+       
+    <div id='foto'class="fotos">
+        <div class="fotos-contente">
+            <div class="carousel">
+                <div>
+                    <img src="https://i.pinimg.com/736x/f1/5b/fa/f15bfaf85305a6e13006c3ba5663c404.jpg" alt="">
+                </div>
+                <div>
+                    <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg" alt="">
+                </div>
+                <div>
+                    <img src="https://marketingcomcafe.com.br/wp-content/uploads/2017/12/banco-imagens-gratis.png" alt="">
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="inicio" id="inicio">
+        <div class="inicio-contente">
+            <h1>Bem-vindos ou site SÓ O GAGÁ SALVA</h1>
+            <p>REGRAS DO GRUPO:</p>
+            <P>Art. 1- O grupo foi feito para criar desavenças, intrigas e  destruir a auto-estima dos integrantes;</P>
+            <p>Art. 2- Sempre que possível colocar em xeque a masculinidade dos participantes;</p>
+            <p>Art. 3- Execrar participantes com posições políticas que não sejam de acordo com a maioria do grupo;</p>
+            <p>Art. 4- Aqui não existe democracia! Aqui o golpe rola solto. A injustiça é permitida;</p>
+            <p>Art. 5- Também é permitido humilhar , ridicularizar, xingar e se utilizar o pior da língua;</p>
+        </div>    
+            
     </div>
 
-   
-       <!-- <div class="fotoIntegrantes" id="foto">
-            <div class="conteinerfoto">
-                <img src="fotos mini/imagem-dos-integrantes.jpg" alt="integrantes">
-            </div> 
-        </div>-->
-        <div id='foto'class="fotos-contente" style="padding-top: 60px;">
-            <div class="fotos">
-                <div class="carousel">
-                        <div>
-                            <img src="https://www.artesanatototal.com/wp-content/uploads/2019/03/Imagens-para-papel-de-parede14.jpg" alt="">
-                        </div>
-                        <div>
-                            <img src="https://i.pinimg.com/736x/f1/5b/fa/f15bfaf85305a6e13006c3ba5663c404.jpg" alt="">
-                        </div>
-                        <div>
-                            <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg" alt="">
-                        </div>
-                        <div>
-                            <img src="https://marketingcomcafe.com.br/wp-content/uploads/2017/12/banco-imagens-gratis.png" alt="">
-                        </div>
+    <div id="participantes" class="participantes">
+        <h1>Participantes</h1>
+        <div class="participantes-contente">
+            <div onclick="usuario(0)" class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+            <div class="participante">
+                <img src="fotos mini/adina.jpg" alt="#">
+                <h2>nome</h2>
+            </div>
+        </div>
+        <div onclick="fechar()" id='teste' class="teste">
+            <div class="testeContent">
+                <div class="fechar"><button onclick="fechar()">fechar</button></div>
+                <div class="telaUser">
+                    <img id="imgUser" class="imgUser" src="">
+                    <p id="pUser"></p>
                 </div>
             </div>
         </div>
-    <!--<section class="inicio" id="inicio">
-            
-                <h1>Bem-vindos ou site exclusivo SÓ O GAGÁ SALVA</h1>
-                <pre>    
-            REGRAS DO GRUPO:
-            
-            Art. 1- O grupo foi feito para criar desavenças, intrigas e  destruir a auto-estima dos integrantes;
-            
-            Art. 2- Sempre que possível colocar em xeque a masculinidade dos participantes;
-            
-            Art. 3- Execrar participantes com posições políticas que não sejam de acordo com a maioria do grupo;
-            
-            Art. 4- Aqui não existe democracia! Aqui o golpe rola solto. A injustiça é permitida;
-            
-            Art. 5- Também é permitido humilhar , ridicularizar, xingar e se utilizar o pior da língua;</pre>
-            
-    </section>-->
+    </div>
+    <!--<div class="gametags" id="gametag">
 
-        <div id="participantes" class="participantes">
-            <h1>Participantes</h1>
-            <div class="participantes-contente">
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
+        <div id="Itens" class="conteinerGametag">  
+        <input type="text" id="txtBusca" placeholder="Digite aqui um valor para pesquisar..."/>  
+            <div id="Itens" class="tag-contente">
+                <div class="tag">
+                    <li id="link1" class="tag-nome">testando</li>
+                    <span><img data-clipboard-target="#link1" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
                 </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
+                <div class="tag">
+                    <li id="link2" class="tag-nome">testandugyuoo</li>
+                    <span><img data-clipboard-target="#link2" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
                 </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
+                <div class="tag">
+                    <li id="link1" class="tag-nome">testando</li>
+                    <span><img data-clipboard-target="#link1" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
                 </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
-                </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
-                </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
-                </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
-                </div>
-                <div class="participante">
-                    <img src="fotos mini/adina.jpg" alt="#">
-                    <h2>nome</h2>
+                <div class="tag">
+                    <li id="link2" class="tag-nome">testandugyuoo</li>
+                    <span><img data-clipboard-target="#link2" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
                 </div>
             </div>
         </div>
-        <div class="gametags" id="gametag">
-    
-            <div id="Itens" class="conteinerGametag">  
-            <input type="text" id="txtBusca" placeholder="Digite aqui um valor para pesquisar..."/>  
-                <div id="Itens" class="tag-contente">
-                    <div class="tag">
-                        <li id="link1" class="tag-nome">testando</li>
-                        <span><img data-clipboard-target="#link1" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
-                    </div>
-                    <div class="tag">
-                        <li id="link2" class="tag-nome">testandugyuoo</li>
-                        <span><img data-clipboard-target="#link2" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
-                    </div>
-                    <div class="tag">
-                        <li id="link1" class="tag-nome">testando</li>
-                        <span><img data-clipboard-target="#link1" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
-                    </div>
-                    <div class="tag">
-                        <li id="link2" class="tag-nome">testandugyuoo</li>
-                        <span><img data-clipboard-target="#link2" class="copiar btn"src="icones/copiarRed.jpg" alt="#"></span>
-                    </div>
-                    
-                    
-                    
-                    
-                </div>
-            </div>
-        </div> 
-        <div id='arquivos'class="arquivos">
-            <div class="arquivos-content">
-                <h1>Arquivos</h1>
-            <table id="tabela">
-                <tr>
-                    <th class="cabecalho">Nome</th>
-                    <th class="cabecalho">Tipo</th>
-                    <th class="cabecalho">Tamanho(Bytes)</th>
-                    <th class="cabecalho">Data de modificação</th>
-                </tr>    
-            </table>
-            </div>
+    </div> -->
+    <div id='arquivos'class="arquivos">
+        <div class="arquivos-content">
+            <h1>Arquivos</h1>
+        <table id="tabela">
+            <tr>
+                <th class="cabecalho">Nome</th>
+                <th class="cabecalho">Tipo</th>
+                <th class="cabecalho">Tamanho(Bytes)</th>
+                <th class="cabecalho">Data de modificação</th>
+            </tr>    
+        </table>
         </div>
+    </div>
        <!-- <div id='fotos'class="fotos-contente">
             <div class="fotos">
                 <div class="carousel">
@@ -216,10 +201,6 @@ include("data.php");
         </div>
     </div>
 
-   <!-- <div class="rodape">
-        rodape
-    </div>-->
-   
 <script src="JS/script.js"></script>
 <script src="JS/users.js"></script>
    
